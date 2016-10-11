10cdf4599fe34d6005e4a107d32c0c12c81f7d4f
Antonio Alfonso Pujante <aalfonso@stratio.com>
2016-10-11 09:30:34 +0200

    check to make sure universe contains frameworks

--- bin/packages.sh
+++ bin/packages.sh
@@ -2,4 +2,8 @@
 
 BASEDIR=`dirname $0`/..
 
-cd docker/local-universe && make base && make local-universe
+cd docker/local-universe && make base && make local-universe 2>&1 > /tmp/mesos_offline_universe_generation
+cat /tmp/mesos_offline_universe_generation
+if [[ ! -z `cat /tmp/mesos_offline_universe_generation | grep "These packages are not included in the image"` ]]; then
+        exit -1
+fi
