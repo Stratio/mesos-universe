a707d23c5796ed38dcefd62f1c299e0ff8efabfe
antonioalf <aalfonso@stratio.com>
2016-10-18 20:07:41 +0200

    add versionning to offline universes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
 BASEDIR=`dirname $0`/..
-cd docker/local-universe && curl -sS -u stratio:${NEXUSPASS} --upload-file local-universe.tar.gz http://sodio.stratio.com/nexus/content/sites/paas/offline-universe/mesos/
+VERSION=`cat $BASEDIR/VERSION`
+
+cd docker/local-universe && curl -sS -u stratio:${NEXUSPASS} --upload-file local-universe-${VERSION}.tar.gz http://sodio.stratio.com/nexus/content/sites/paas/offline-universe/mesos/
