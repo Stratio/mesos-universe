eddcdbe5a96d3aae635ce602daa390004fd1e08c
antonioalf <aalfonso@stratio.com>
2016-10-27 17:23:54 +0200

    remove cat from offline universe generation

--- bin/packages.sh
+++ bin/packages.sh
@@ -4,7 +4,6 @@ BASEDIR=`dirname $0`/..
 VERSION=`cat $BASEDIR/VERSION`
 
 cd docker/local-universe && make base && make local-universe 2>&1 | tee /tmp/mesos_offline_universe_generation
-cat /tmp/mesos_offline_universe_generation
 if [[ ! -z `cat /tmp/mesos_offline_universe_generation | grep "These packages are not included in the image"` ]]; then
         exit -1
 fi
