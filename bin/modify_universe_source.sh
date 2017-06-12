82b1fbf757717aa5f2e3d0bf68d3fe25725fc10e
antonioalf <aalfonso@stratio.com>
2017-06-12 11:42:38 +0200

    fixes for offline universe generation

--- bin/modify_universe_source.sh
+++ bin/modify_universe_source.sh
@@ -4,7 +4,7 @@ SOURCE=$1
 NAME_ADDITION=$2
 
 # modify docker/local-universe/Makefile
-sed -e "s/master.mesos/$SOURCE/g" -e "s/local-universe/local-$NAME_ADDITION-universe/g" -i docker/local-universe/Makefile
+sed -e "s/master.mesos/$SOURCE/g" -e "s/local-universe.tar/local-$NAME_ADDITION-universe.tar/g" -i docker/local-universe/Makefile
 
 # modify scripts/local-universe.py
 sed "s/master.mesos/$SOURCE/g" -i scripts/local-universe.py
