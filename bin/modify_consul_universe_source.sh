1d94592b71277a86ca531e674b4161ecab212770
Antonio Alfonso Pujante <aalfonso@stratio.com>
2017-06-12 11:43:46 +0200

    fixes for offline universe generation (#24)

--- bin/modify_consul_universe_source.sh
+++ bin/modify_consul_universe_source.sh
@@ -3,7 +3,7 @@
 SOURCE=$1
 
 # modify docker/local-universe/Makefile
-sed -e "s/universe.service.consul/$SOURCE/g" -e "s/local-consul-universe/local-universe/g" -i docker/local-universe/Makefile
+sed -e "s/universe.service.consul/$SOURCE/g" -e "s/local-consul-universe.tar/local-universe.tar/g" -i docker/local-universe/Makefile
 
 # modify scripts/local-universe.py
 sed "s/universe.service.consul/$SOURCE/g" -i scripts/local-universe.py
